b88a7691944d181961963319ab6055542955b182
Laura Schulz <lrs8810@gmail.com>
2020-03-02 18:25:07 -0700

    Add spec for get all olympians endpoint

---
+++ spec/requests/api/v1/olympians_request_spec.rb
@@ -0,0 +1,18 @@
+require 'rails_helper'
+
+RSpec.describe 'Olympians API' do
+  before(:each) do
+    create_list(:olympian, 3)
+  end
+
+  it 'sends a list of olympians' do
+    get '/api/v1/olympians'
+
+    expect(response).to be_successful
+
+    olympians = JSON.parse(response.body)
+
+    expect(olympians.count).to eq(3)
+    expect(olympians).to have_key("olympians")
+  end
+end
