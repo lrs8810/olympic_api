57124a18aef6fc2dbfdde17893391a06c9d78bd4
Laura Schulz <lrs8810@gmail.com>
2020-03-03 15:53:52 -0700

    Refactor index action and add private method check_params

--- app/controllers/api/v1/olympians_controller.rb
+++ app/controllers/api/v1/olympians_controller.rb
@@ -1,10 +1,17 @@
 class Api::V1::OlympiansController < ApplicationController
   def index
-    if params[:age]
-      olympians = FormatOlympians.new(Olympian.youngest)
-    else
-      olympians = FormatOlympians.new(Olympian.all)
-    end
+    olympians = check_params(params)
     render json: olympians
   end
+
+  private
+    def check_params(params)
+      if request.query_string.empty?
+        olympians = FormatOlympians.new(Olympian.all)
+      elsif params[:age] == 'youngest'
+        olympians = FormatOlympians.new(Olympian.youngest)
+      elsif params[:age] == 'oldest'
+        olympians = FormatOlympians.new(Olympian.oldest)
+      end
+    end
 end
