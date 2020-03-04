20a0566c20e85117c3fe13212794768e2b5c104e
Laura Schulz <lrs8810@gmail.com>
2020-03-03 17:46:44 -0700

    Add additional expectations to check all avgs are Floats

--- spec/requests/api/v1/olympian_stats_request_spec.rb
+++ spec/requests/api/v1/olympian_stats_request_spec.rb
@@ -8,7 +8,7 @@ RSpec.describe 'Olympian Stats API' do
     create(:olympian, age: 40, weight: 80, sex: 'F')
     create(:olympian, age: 35, weight: 100, sex: 'M')
     create(:olympian, age: 19, weight: 165, sex: 'M')
-    
+
     get '/api/v1/olympian_stats'
 
     expect(response).to be_successful
@@ -20,12 +20,16 @@ RSpec.describe 'Olympian Stats API' do
     expect(stats_res).to be_a(Hash)
     expect(stats_res[:olympian_stats]).to be_a(Hash)
     expect(stats_res[:olympian_stats]).to have_key(:total_competing_olympians)
+    expect(stats_res[:olympian_stats][:total_competing_olympians]).to be_a(Integer)
     expect(stats_res[:olympian_stats]).to have_key(:average_weight)
     expect(stats_res[:olympian_stats]).to have_key(:average_age)
+    expect(stats_res[:olympian_stats][:average_age]).to be_a(Float)
     expect(stats_res[:olympian_stats][:average_weight]).to be_a(Hash)
     expect(stats_res[:olympian_stats][:average_weight]).to have_key(:unit)
     expect(stats_res[:olympian_stats][:average_weight][:unit]).to eq('kg')
     expect(stats_res[:olympian_stats][:average_weight]).to have_key(:male_olympians)
+    expect(stats_res[:olympian_stats][:average_weight][:male_olympians]).to be_a(Float)
     expect(stats_res[:olympian_stats][:average_weight]).to have_key(:female_olympians)
+    expect(stats_res[:olympian_stats][:average_weight][:female_olympians]).to be_a(Float)
   end
 end
