f88b7e0b34e73e9affa00ca4a246c8ab48ee3349
Laura Schulz <lrs8810@gmail.com>
2020-03-04 01:26:22 -0700

    Refactor index action and add private methods

--- app/controllers/api/v1/olympians_controller.rb
+++ app/controllers/api/v1/olympians_controller.rb
@@ -1,17 +1,38 @@
 class Api::V1::OlympiansController < ApplicationController
   def index
-    olympians = check_params(params)
-    render json: olympians
+    if request.query_string.empty?
+      show_all_olympians
+    elsif !age_params.empty? && check_params
+      show_olympian_based_on_age
+    else
+      render json: error_message, status: 400
+    end
   end
 
   private
-    def check_params(params)
-      if request.query_string.empty?
-        FormatOlympians.new(Olympian.all)
-      elsif params[:age] == 'youngest'
-        FormatOlympians.new(Olympian.youngest)
+    def show_all_olympians
+      olympians = FormatOlympians.new(Olympian.all)
+      render json: olympians
+    end
+
+    def show_olympian_based_on_age
+      if params[:age] == 'youngest'
+        olympians = FormatOlympians.new(Olympian.youngest)
       elsif params[:age] == 'oldest'
-        FormatOlympians.new(Olympian.oldest)
+        olympians = FormatOlympians.new(Olympian.oldest)
       end
+      render json: olympians
+    end
+
+    def check_params
+      params[:age] == 'youngest' || params[:age] == 'oldest'
+    end
+
+    def age_params
+      params.permit(:age)
+    end
+
+    def error_message
+      { 'error': 'The request you made is invalid. Please pass `age=youngest` or `age=oldest` in the query parameters.' }
     end
 end
